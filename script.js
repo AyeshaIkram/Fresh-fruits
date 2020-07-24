7facf737333a6f87d00d56a2f2b4f25ca9c5267b
AyeshaIkram <68427480+AyeshaIkram@users.noreply.github.com>
2020-07-24 11:14:33 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,87 @@
+// For making bills
+
+function updateBill() {
+    const totallychee = lychee * 35;
+    const totalmango = mango * 20;
+    const totaldragon = dragon * 25;
+    let bill = parseInt(document.querySelector('#bill input').value);
+
+    bill = totallychee + totalmango + totaldragon;
+    document.querySelector('#bill input').value = bill;
+}
+// For buying apples
+
+
+
+function addlychee() {
+
+    let pocket = parseInt(document.querySelector('#input input').value);
+    let bill = parseInt(document.querySelector('#bill input').value);
+
+    if (pocket < bill + 35) {
+      alert("Enter More Money")
+    } else {
+        document.querySelector('#lychee-number').value = ++lychee;
+        updateBill();
+    }
+}
+let lychee = 0;
+function xlychee() {
+  if(lychee == 0){
+    alert("You don't have any lychee")
+  }
+  else{
+    document.querySelector('#lychee-number').value = --lychee;
+    updateBill();
+}
+}
+// For buying oranges
+
+function addmango() {
+    let pocket = parseInt(document.querySelector('#input input').value);
+    let bill = parseInt(document.querySelector('#bill input').value);
+
+    if (pocket < bill + 20) {
+      alert("Enter More Money")
+    } else {
+        document.querySelector('#mango-number').value = ++mango;
+        updateBill();
+    }
+}
+let mango = 0;
+
+function xmango() {
+  if(mango == 0){
+    alert("You don't have any Mango!");
+  }
+  else{
+    document.querySelector('#mango-number').value = --mango;
+    updateBill();
+  }
+}
+// For buying bananas
+
+
+function addDragonfruit() {
+    let pocket = parseInt(document.querySelector('#input input').value);
+    let bill = parseInt(document.querySelector('#bill input').value);
+
+    if (pocket < bill + 25) {
+      alert("Enter More Money")
+    } else {
+        document.querySelector('#dragon-number').value = ++dragon;
+        updateBill();
+    }
+
+}
+let dragon = 0;
+function xDragonfruit() {
+  if(dragon == 0){
+    alert("You don't have any Dragons!");
+  }
+  else{
+    
+    document.querySelector('#dragon-number').value = --dragon;
+    updateBill();
+    }
+}
